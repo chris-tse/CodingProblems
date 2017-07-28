194f6f1075e266b4f6476ae4e2e5fdf0ca0d9fcb
Christopher Tse <christopher-tse@outlook.com>
2017-07-28 05:20:54 -0500

    Update pyramid solution

--- solutions/pyramid.js
+++ solutions/pyramid.js
@@ -1,43 +1,48 @@
+// Solution using string replacement and JavaScript array shifting
 const pyramid1 = base => {
-    const startString = num => {
-        let start = ""
-        for (let i = 0; i < num - 1; i++) {
-            start += " "
-        }
-        return start + "0"
+    // Initialize array to store strings to print
+    let strings = []
+
+    // Create base line
+    let str = Array(base+1).join("0")
+    strings.push(str)
+
+    // For each line, replace the character on either end
+    // with a space and push to beginning of array
+    for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+        str = str.substr(0, i) + ' ' + str.substr(i + 1);
+        str = str.substr(0, j) + ' ' + str.substr(j + 1);
+        strings.unshift(str)
     }
+
+    // Check if first line is empty string for pyramids that have point of width 2
+    for (let i = base % 2 === 0 ? 1 : 0; i < strings.length; i++) {
+        console.log(strings[i])
+    }
+}
+
+// Solution calculating number of required characters and spaces per line
+const pyramid2 = base => {
+    // Functions to create strings of spaces or 0's of length num
     const chars = num => Array(num+1).join("0")
     const spaces = num => Array(num+1).join(" ")
 
     const even = base % 2 === 0 ? true : false
     const lines = parseInt(base/2) + 1
 
-
+    // Calculate number of spaces and characters for the left half
+    // then duplicate to the right and print it
     for (let line = even ? 1 : 0; line < lines; line++) {
         let charStr = chars(line)
         let spaceStr = spaces(lines - line - 1)
         let chunk = spaceStr + charStr
         let currLine = even ? chunk : chunk + "0"
-        currLine += [...chunk].reverse().join("")
+        currLine += charStr + spaceStr
         console.log(currLine)
     }
 }
 
-const pyramid2 = base => {
-    let strings = []
 
-    let str = Array(base+1).join("0")
-    strings.push(str)
-    for(let i = 0, j = str.length - 1; i < j; i++, j--) {
-        str = str.substr(0, i) + ' ' + str.substr(i + 1);
-        str = str.substr(0, j) + ' ' + str.substr(j + 1);
-        strings.unshift(str)
-    }
-
-    for (let i = base % 2 === 0 ? 1 : 0; i < strings.length; i++) {
-        console.log(strings[i])
-    }
-}
 
 
 const pyramid3 = base => {
@@ -57,18 +62,3 @@ const pyramid3 = base => {
         console.log(str)
     }
 }
-
-pyramid(9)
-
-
-//             4 spaces 0 char line 0
-//    00       3 spaces 1 char line 1
-//   0000      2 spaces 2 char line 2
-//  000000     1 spaces 3 char line 3
-// 00000000    0 spaces 4 char line 4
-
-//     0      4 spaces 0 char
-//    000     3 spaces 1 char
-//   00000    etc
-//  0000000
-// 000000000
