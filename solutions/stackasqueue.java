a936fc3d5b6f54c7f5c66b10439f790b702cad95
Christopher Tse <christopher-tse@outlook.com>
2017-07-28 05:32:01 -0500

    Add stackasqueue solution in java

---
+++ solutions/stackasqueue.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        while(queue.peek() != x) {
+            queue.add(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return (queue.size() > 0) ? false : true;
+    }
+}
